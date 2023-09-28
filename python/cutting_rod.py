ce64103b82cc1c1d0e6b20631f5dd52abc252a2f
Guillaume AZAM <guillaumeazam@MacBook-Pro-de-TALAN.local>
2023-09-28 23:41:33 +0200

    add read me

--- python/cutting_rod.py
+++ python/cutting_rod.py
@@ -3,6 +3,6 @@ from typing import List
 
 # Function to find the best way to cut a rod of length `n`
 # where the rod of length `i` has a cost `price[i-1]`
-def rod_cut(price: List[int], n: int) -> int:
+def rod_cut(prices: List[int], n: int) -> int:
     pass
 
